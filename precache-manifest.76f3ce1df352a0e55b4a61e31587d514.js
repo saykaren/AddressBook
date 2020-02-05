9002f9dc8566870aa66cf62687b2dddac59d8af2
Karen Garcia <kgarcia@i3screen.com>
2020-02-04 20:15:39 -0700

    Updates

---
+++ precache-manifest.76f3ce1df352a0e55b4a61e31587d514.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff9d6ca555a796a031627ec282fb5950",
+    "url": "/AddressBook/index.html"
+  },
+  {
+    "revision": "30e142fa53077449b14a",
+    "url": "/AddressBook/static/css/main.03b6363f.chunk.css"
+  },
+  {
+    "revision": "576ef0a81e8a68ca6081",
+    "url": "/AddressBook/static/js/2.882c6436.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/AddressBook/static/js/2.882c6436.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30e142fa53077449b14a",
+    "url": "/AddressBook/static/js/main.51002642.chunk.js"
+  },
+  {
+    "revision": "aa254103c604f3ba0f58",
+    "url": "/AddressBook/static/js/runtime-main.363fa443.js"
+  }
+]);
